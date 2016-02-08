6ce832bb3a77ddf1d3cdbf069ba4ea18f35b8828
chono91 <chono91@gmail.com>
2016-02-08 14:00:54 -0500

    better generator, still broken

--- psdirbuster.ps1
+++ psdirbuster.ps1
@@ -2,30 +2,49 @@
 
 #seed
     #Pure brute force option
+    #instead of using ascii as you see below, we can just increment through the array using I.. this is stupid but works for now
     #allow different libraries to be selected
     #allow self loaded libraries
 function getseed($a) {
-    
-    $input = [string]$a
-    write-host $a #debug
+    $input = $a
     $alphachars = @("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
-    $alphachars = @([byte][char]"a", [byte][char]"z")
+    #$alphachars = @([byte][char]"a", [byte][char]"z")
     $numchars = 0,1,2,3,4,5,6,7,8,9
-    $numchars = @([byte][char]"0", [byte][char]"9")
+    #$numchars = @([byte][char]"0", [byte][char]"9")
     $specchars = @('!','@','#','$','%','^','&','*','(',')','_','-','+','=','~','`','<','>',',','.','?','/',"'",'"',':',';','{','[',']','}','|','\')
-    $htmlchars = @([byte][char]'!', [byte][char]'~')
+    #$htmlchars = @([byte][char]'!', [byte][char]'~')
 
-    $seed = $htmlchars
-    #$check = 1 #loop seed
-    #$results = @()
+    $seed = $alphachars
 
-    #$input = "9994991" #test input debug
-    $inputarray = [char[]]$input #get char array from string
-    $asciin = [byte][char]$inputarray[$inputarray.count-1] #get ascii from last char in input
+    [System.Collections.ArrayList]$inputarray = [char[]]$input
+    $lastchar = $inputarray[-1]
+    $lastchar = "$lastchar"
+
+    if($seed::indexof($seed,$lastchar) -eq -1) {
+        Write-Host "ERROR does not exist in char set"
+        } #check if the input is a valid char
+
+    elseif($lastchar -ne $seed[-1]){
+        $inputarray[-1] = $seed[$seed::indexof($seed,$lastchar)+1]
+        $inputarray = ($inputarray | ?{$_})
+        }
+
+    else { 
+        $inputarray[-1] = $seed[0]
+        $inputarray+=$seed[0]
+        }
+
+ #   write-host $inputarray[0] "," $inputarray[1] "," $inputarray[2] "final"
+    return $inputarray
+}
+
+    <#old asciinumber dependent
+
     if($asciin -lt $seed[1] -and $asciin -ge $seed[0]){ #test if last char is greater than the lowest valid input and less than the highest valid input -1 (eg. between a and y)
         $asciin ++ #increment by 1 y-> z
         $inputarray[$inputarray.Count-1] = [char]$asciin #over write the last char in the string
-        write-host $inputarray #write out debug
+        write-host "HERE1" $inputarray #write out debug
+        return $inputarray
         }
 
     elseif($asciin -eq $seed[1]){ #check if last char is equal to the greatest valid input (eg. z)
@@ -43,11 +62,13 @@ function getseed($a) {
             $inputarray[$inputarray.count-$i] = [char]$ascii #save eg. zzzgzzz -> zzzfaaa
 
             }
-        write-host $inputarray
+        write-host "HERE2" $inputarray #debug
+        return $inputarray
         }
 
     else{write-host "ERROR"}
 }
+#>
 <# loop to create library for brute force, too big
 $d1 = @()
 $d2 = @()
@@ -77,22 +98,34 @@ write-host "D1: "$d1
 write-host "D2: "$d2
 #>
 
-<#
-while ($check -le 3) {#check loop
-    $testUrl = "https://www.google.com/654654" #debug testing url
+$check = 1
+
+while ($check -le 4) {#check loop
+    $baseUrl = "https://www.google.com/654654" #debug testing url
+    $maxlength = 6
+    $searchUrl = 'aa'
+    while($searchUrl.count -le $maxLength){
+        
+ #       write-host $searchurl " before"
+        $searchUrl = getseed($searchUrl)
+        write-host $searchUrl
+  #      write-host $searchUrl[0] " after1"
+   #     write-host $searchUrl[1] " after2"
+    #    write-host $searchUrl[2] " after3"
+
+        $dirObject = new-object -TypeName PSObject #create on object to store the response in
+        $dirObject | Add-Member -type NoteProperty -name URL -Value $testUrl  #store the url being tested
     
-    $dirObject = new-object -TypeName PSObject #create on object to store the response in
-    $dirObject | Add-Member -type NoteProperty -name URL -Value $testUrl  #store the url being tested
+        try {$response = Invoke-WebRequest -Uri $testUrl -method Get -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer)} #try the webrequest (400 codes error)
+        catch {$response = $_} # Catch any 400 codes
     
-    try {$response = Invoke-WebRequest -Uri $testUrl -method Get -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer)} #try the webrequest (400 codes error)
-    catch {$response = $_} # Catch any 400 codes
+        if ($response.StatusCode) {$dirObject | Add-Member -type NoteProperty -name response -Value $response.StatusCode} #if not a 400 code, store the code 
+        else {$dirObject | Add-Member -type NoteProperty -name response -Value $response} #If error code, store that
     
-    if ($response.StatusCode) {$dirObject | Add-Member -type NoteProperty -name response -Value $response.StatusCode} #if not a 400 code, store the code 
-    else {$dirObject | Add-Member -type NoteProperty -name response -Value $response} #If error code, store that
-    
-    $check ++ #loop increment
-    $results += $dirObject
-    
+        $check ++ #loop increment
+        $results += $dirObject
+        
+    }
     #------------------#
     #the key could be the url, the dirobject could store the other info, something to consider
     #$key = $testUrl
@@ -101,4 +134,3 @@ while ($check -le 3) {#check loop
 }
    
    #Invoe-WebRequest -Uri http://www.google.com -method Get -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer)
-   #>
